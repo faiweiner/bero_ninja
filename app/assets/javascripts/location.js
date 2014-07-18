48d38c95d051884dacd5fab9c32a5636533869a6
bill <billsearle@me.com>
2014-07-18 19:31:40 +1000

    fixed location.js after pushing last time and not testing, just like we were told...

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
 
-	if (navigator.geolocation) {
-	  var timeoutVal = 4500;
-	  navigator.geolocation.getCurrentPosition(
-	    displayPosition, 
-	    displayError
-	    // { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
-	  );
-	}	else {
-	  alert("Geolocation is not supported by this browser");
-	}
+	var locations = function() {
 
-	function displayPosition(position) {
-	  console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
-	};
+		if (navigator.geolocation) {
+		  var timeoutVal = 4500;
+		  navigator.geolocation.getCurrentPosition(
+		    displayPosition, 
+		    displayError
+		    // { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+		  );
+		}	else {
+		  alert("Geolocation is not supported by this browser");
+		}
 
-	function displayError(error) {
-	  var errors = { 
-	    1: 'Permission denied',
-	    2: 'Position unavailable',
-	    3: 'Request timeout'
-	  };
-	  alert("Error: " + errors[error.code]);
+		function displayPosition(position) {
+		  console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+		};
+
+		function displayError(error) {
+		  var errors = { 
+		    1: 'Permission denied',
+		    2: 'Position unavailable',
+		    3: 'Request timeout'
+		  };
+		  alert("Error: " + errors[error.code]);
+		};
 	};
-window.setInterval(locations, 5000);
+		window.setInterval(locations, 5000);
 });
 
 // AJAX BASIC GUIDE FROM JOEL
