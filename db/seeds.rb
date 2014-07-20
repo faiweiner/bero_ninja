e0b434a9bffe50f93a8c405b8b4a1c9a033ad322
bill <billsearle@me.com>
2014-07-20 14:06:48 +1000

    added 3 user login's to seeds. Cannot get login or logout working :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
-u1 = User.create :username => 'Ros'
+User.destroy_all
+
+u1 = User.create :username => "Ros", :password => "ros", :password_confirmation => "ros"
+u2 = User.create :username => "Fai", :password => "fai", :password_confirmation => "fai"
+u3 = User.create :username => "Bill", :password => "bill", :password_confirmation => "bill"
