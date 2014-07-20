c54fd661c2ac681238c27eaf0a934ae6a9296630
Rosalind Skinner <ros.skinner@gmail.com>
2014-07-20 20:54:20 +1000

    map options, styles and add marker to map

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -9,20 +9,81 @@ var display_map = function (latitude, longitude, zoom) {
   var mapOptions = {
     center: new google.maps.LatLng(latitude, longitude),
     zoom: zoom,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "color": "#e0efef"
+        }]
+    }, {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "hue": "#1900ff"
+        }, {
+            "color": "#c0e8e8"
+        }]
+    }, {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill"
+    }, {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [{
+            "lightness": 100
+        }, {
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "water",
+        "stylers": [{
+            "color": "#7dcdcd"
+        }]
+    }, {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "lightness": 700
+        }]
+    }]
   };
 
   var canvas = $('#map_canvas').get(0);
 
   map = new google.maps.Map(canvas, mapOptions);
+
+  google.maps.event.addListener(map, 'click', function(e) {
+    placeMarker(e.latLng, map);
+  });
+
+  function placeMarker(position, map) {
+    var marker = new google.maps.Marker({
+    position: position,
+    map: map
+  });
+  map.panTo(position);
+  }
 };
 
-var add_marker = function (latitude, longitude, address) {
-  var latlng = new google.maps.LatLng(latitude, longitude);
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: map,
-    address: address
+  var add_marker = function (latitude, longitude, address) {
+    var latlng = new google.maps.LatLng(latitude, longitude);
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      address: address
   });
 
 };
