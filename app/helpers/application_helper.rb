574eb0101079ea8fa4fd6f8c82647e511e13a7a0
fai.weiner <fai.weiner@gmail.com>
2014-07-20 22:35:52 +1000

    phase 1.1 bootstrap overhaul

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def smartnav
     links = ''
-    links += bero
-
     if @current_user.present?
       links += "<li>"
       links += link_to('Logout', login_path, :data => {:method => :delete, :confirm => 'Really logout?'})
@@ -11,14 +9,10 @@ module ApplicationHelper
       links += link_to("Welcome #{@current_user.username}", user_path(@current_user))
       links += "</li>"
     else
-      links += "<div>#{ link_to('Sign up', new_user_path) }</div>"
-      links += "<div>#{ link_to('Sign in', new_session_path) }</div>"
+      links += "<li>#{ link_to('Sign up', new_user_path) }</li>"
+      links += "<li>#{ link_to('Sign in', new_session_path) }</li>"
     end
 
     links
-  end
-
-  def bero
-    "<div class='doo-doo'>#{ link_to('Bero', root_path) }</div>"
   end
 end
