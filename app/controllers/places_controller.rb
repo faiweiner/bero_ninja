583a2a9d7b2873113fc97db4e0b81b80dd6ed795
Rosalind Skinner <ros.skinner@gmail.com>
2014-07-21 12:17:50 +1000

    places/new

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,15 +2,30 @@ class PlacesController < ApplicationController
 
   def index
     @places = @current_user.places
-    
+
+  end
+
+  def new
+    @places = @current_user.places
+    @place = Place.new
   end
 
   def create
-
+    @place = Place.new place_params
+    if @place.save
+      @current_user.places << @place
+      render :json => @place
+    end
   end
 
+
+
   private
-    def check_if_logged_in
-      redirect_to(root_path) unless @current_user
-    end
+  def place_params
+    params.require(:place).permit(:address, :category, :favorite)
+  end
+
+  def check_if_logged_in
+    redirect_to(root_path) unless @current_user
+  end
 end
