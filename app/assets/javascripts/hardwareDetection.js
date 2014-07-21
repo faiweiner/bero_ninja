7e760c13a36580ed8c39a9641e8d1ede8489d21a
fai.weiner <fai.weiner@gmail.com>
2014-07-22 09:20:53 +1000

    added the detectByOrientation to hardware detection.js

--- app/assets/javascripts/hardwareDetection.js
+++ app/assets/javascripts/hardwareDetection.js
@@ -1,10 +1,18 @@
 var screenDetect = function() {
 	// detects the size of the screen and, if applicable, disables/enables approriate elements within the page
-  if ($('#desktopTest').is(':hidden')) {
-  	return "mobile";
-  } else {
-    return "desktop";
-   };
+	if ($('#desktopTest').is(':hidden')) {
+		return "mobile";
+	} else {
+		return "desktop";
+	};
+};
+
+var screenDetectByOrientation = function() {
+	if Modernizr.hasEvent('DeviceOrientation') { 		
+		return "mobile";
+	} else { 		
+		return "desktop"; 	
+	};
 };
 
 // Hide element if on Desktop
@@ -27,17 +35,17 @@ var elementForDesktop = function(element) {
 };
 
 var elementAppendText = function(element, newClass, message) {
-  $(element).addClass(newClass);
-  $(element).text(message);
+	$(element).addClass(newClass);
+	$(element).text(message);
 };
 
 var hardwareDetection = function() {
-  if (navigator.geolocation) {
-    elementAppendText("#hardware-error", "alert alert-success", "Geolocation detected - you\'re ready to Bero!");
-  } else {
-  	elementAppendText("#hardware-error", "alert alert-danger", "Geolocation not available - please use another geolocation-enabled device.");
-    // The following code hides elements (features) when hardware is incompatible
-  	elementAppendText("#find-places", "disabled", null);
-    elementAppendText("#find-friends", "disabled", null);
-  };
+	if (navigator.geolocation) {
+		elementAppendText("#hardware-error", "alert alert-success", "Geolocation detected - you\'re ready to Bero!");
+	} else {
+		elementAppendText("#hardware-error", "alert alert-danger", "Geolocation not available - please use another geolocation-enabled device.");
+		// The following code hides elements (features) when hardware is incompatible
+		elementAppendText("#find-places", "disabled", null);
+		elementAppendText("#find-friends", "disabled", null);
+	};
 };
