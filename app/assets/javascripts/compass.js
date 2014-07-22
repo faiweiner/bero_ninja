b9c66cb467971d7a800d58cef30b14fccd1cb1c3
bill <billsearle@me.com>
2014-07-22 15:46:24 +1000

    more compass.js testing. + compass.bearing

--- app/assets/javascripts/compass.js
+++ app/assets/javascripts/compass.js
@@ -6,14 +6,14 @@ function init() {
                 var alpha;
                 //Check for iOS property
                 if(event.webkitCompassHeading) {
-                  alpha = event.webkitCompassHeading + compass.bearing;
+                  alpha = event.webkitCompassHeading;
                   //Rotation is reversed for iOS
-                  compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+                  compass.style.WebkitTransform = 'rotate(-' + alpha + compass.bearing + 'deg)';
                 }
                 //non iOS
                 else {
-                  alpha = event.alpha + compass.bearing;
-                  webkitAlpha = alpha;
+                  alpha = event.alpha;
+                  webkitAlpha = alpha + compass.bearing;
                   if(!window.chrome) {
                     //Assume Android stock (this is crude, but good enough for our example) and apply offset
                     webkitAlpha = alpha-270;
