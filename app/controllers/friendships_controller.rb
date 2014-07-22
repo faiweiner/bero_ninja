8c1695eb703a2aada103b727b1c7e00bae59391c
fai.weiner <fai.weiner@gmail.com>
2014-07-22 10:17:59 +1000

    starting to build out the View All Friends view (in partials)

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,12 +3,25 @@ class FriendshipsController < ApplicationController
 	end
 
   def create
+  	@friendship = @current_user.friendships.build(:friend_id => params[:friend_id])
+  	if @friendship.save
+  		flash[:notice] = "Friend successfully added."
+  		redirect_to friendships_path
+  	else
+  		render :action => 'new'
+  	end
   end
    
   def new
+
+		# raise params.inspect
   end
 
   def destroy
+  	@friendship = Friendship.find(params[:id])
+  	@friendship.destroy
+  	flash[:notice] = "Successfully deleted friendship."
+  	redirect_to friendships_path
   end
-  
+
 end
