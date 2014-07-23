8d771f41ae49a827efd700936392c5343403f549
bill <billsearle@me.com>
2014-07-24 09:46:39 +1000

    added logging to debug compass

--- app/assets/javascripts/compass.js
+++ app/assets/javascripts/compass.js
@@ -15,6 +15,10 @@ $(document).ready(function() {
 
           window.addEventListener('deviceorientation', function(event) {
                 var alpha;
+
+                var log = (new Date()) + " webKitCompassHeading: " + event.webKitCompassHeading;
+                $('.log').prepend('<p>' + log + '</p>');
+
                 //Check for iOS property
                 if(event.webkitCompassHeading) {
                   
