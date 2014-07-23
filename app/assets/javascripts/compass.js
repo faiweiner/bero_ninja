99d89dcceae03446b94d6bc5b6f0a7f6dfa9cc0f
bill <billsearle@me.com>
2014-07-23 14:27:26 +1000

    v1 ios testing

--- app/assets/javascripts/compass.js
+++ app/assets/javascripts/compass.js
@@ -18,11 +18,12 @@ $(document).ready(function() {
                 //Check for iOS property
                 if(event.webkitCompassHeading) {
                   
-                  alpha = event.webkitCompassHeading - userCompass.bearing;
+                  alpha = event.webkitCompassHeading + userCompass.bearing;
                   // alpha = Math.abs(alpha);
                   $('#alpha').text(event.webkitCompassHeading);
                   $('#accuracy').text(event.webkitCompassAccuracy);
                   $('#alphaWithBearing').text(alpha);
+                  $('#version').text(1);
                   //Rotation is reversed for iOS
                   compass.style.WebkitTransform = 'rotate(-' + alpha  + 'deg)';
                 }
