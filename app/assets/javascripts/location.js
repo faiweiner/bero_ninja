1f897c7e5b8b9c54c7cdc38decc5748589133b74
Rosalind Skinner <ros.skinner@gmail.com>
2014-07-24 19:10:42 +1000

    centering arrow and hiding shit

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -23,51 +23,51 @@ $(document).ready(function() {
 		var user_lat = position.coords.latitude;
 		var user_lng = position.coords.longitude;
 			// AJAX gets location data from device and converts for use in ruby.
-			$.ajax({
-				// Step 1
-				url: stitchURL,
-				type: 'GET',
-				dataType: 'JSON',
-				data: {
-					user_lat: user_lat,
-					user_lng: user_lng
-				},
-				// Step 2
-				success: function(response) {
-					console.log(response);
-					$('#user_lat').text(response.coords.user_lat); // push data to user_lat id on page
-					$('#user_lng').text(response.coords.user_lng); // push data to user_lng id on page
-					$('#bearing').text(response.bearing);
-					$('#distance').text(response.distance);
-					$('#heading').text(response.compass);
-					userCompass.bearing = response.bearing;
-					userCompass.distance = response.distance;
-					userCompass.lat = response.coords.user_lat;
-					userCompass.lng = response.coords.user_lng;
+	$.ajax({
+		// Step 1
+		url: stitchURL,
+		type: 'GET',
+		dataType: 'JSON',
+		data: {
+			user_lat: user_lat,
+			user_lng: user_lng
+	},
+		// Step 2
+		success: function(response) {
+			console.log(response);
+			$('#user_lat').text(response.coords.user_lat); // push data to user_lat id on page
+			$('#user_lng').text(response.coords.user_lng); // push data to user_lng id on page
+			$('#bearing').text(response.bearing);
+			$('#distance').text(response.distance);
+			$('#heading').text(response.compass);
+			userCompass.bearing = response.bearing;
+			userCompass.distance = response.distance;
+			userCompass.lat = response.coords.user_lat;
+			userCompass.lng = response.coords.user_lng;
 
-					console.log(userCompass.distance)
-                var distanceNow = userCompass.distance;
-                userCompass.distanceOrig = userCompass.distanceOrig || distanceNow;
-                // $('#compassContainer').text('original distance: '+ userCompass.distanceOrig + ' distance now: '+ distanceNow)
-                console.log('original distance', userCompass.distanceOrig, 'distance now', distanceNow, userCompass.distanceOrig != distanceNow);
-                // Finding the range of colors in percentage for distance.
-                if ((userCompass.distanceOrig != distanceNow) && (userCompass.distanceOrig > distanceNow)) {
-                  range = 3000 * (1 - (distanceNow/userCompass.distanceOrig));
-                  // return range;
-                  console.log('new range', range, 'distance', distanceNow);
-                } else {
-                  range = 0;
+			console.log(userCompass.distance)
+        var distanceNow = userCompass.distance;
+        userCompass.distanceOrig = userCompass.distanceOrig || distanceNow;
+        // $('#compassContainer').text('original distance: '+ userCompass.distanceOrig + ' distance now: '+ distanceNow)
+        console.log('original distance', userCompass.distanceOrig, 'distance now', distanceNow, userCompass.distanceOrig != distanceNow);
+        // Finding the range of colors in percentage for distance.
+        if ((userCompass.distanceOrig != distanceNow) && (userCompass.distanceOrig > distanceNow)) {
+          range = 3000 * (1 - (distanceNow/userCompass.distanceOrig));
+          // return range;
+          console.log('new range', range, 'distance', distanceNow);
+        } else {
+          range = 1;
 
-                  console.log('some magic ros range', range);
-                }
+          console.log('some magic ros range', range);
+        }
 
-                console.log('range: '+ range);
-                var compassGradient = '-webkit-gradient(radial, 50% 0 , 0, 50% 0,' + parseInt(range) +', from(#FF213D), to(#0E213D))';
-                $('#compass').css('background', compassGradient);
-                console.log(compassGradient);
+        console.log('range: '+ range);
+        var compassGradient = '-webkit-gradient(radial, 50% 0 , 0, 50% 0,' + parseInt(range) +', from(#FF213D), to(#0E213D))';
+        $('#compass').css('background', compassGradient);
+        console.log(compassGradient);
 
-                $('#compass').css({'-webkit-background-clip': 'text'});
-                $('#compass').css({'-webkit-text-fill-color': 'transparent'});
+        $('#compass').css({'-webkit-background-clip': 'text'});
+        $('#compass').css({'-webkit-text-fill-color': 'transparent'});
 
 
 				}
